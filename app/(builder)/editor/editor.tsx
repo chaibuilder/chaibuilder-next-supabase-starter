4c957571436afb76022a5a8c9c5e1bb45d5397b8
amitdhiman5086 <amitdhiman5086@gmail.com>
2026-03-16 10:57:08 +0530

    refactor: update `@chaibuilder/next` dependency and remove the `websocket` prop from the editor

--- app/(builder)/editor/editor.tsx
+++ app/(builder)/editor/editor.tsx
@@ -114,7 +114,6 @@ export default function Editor() {
       apiUrl="api"
       getPreviewUrl={getPreviewUrl}
       getLiveUrl={getLiveUrl}
-      websocket={supabase}
       onLogout={handleLogout}
     />
   );
